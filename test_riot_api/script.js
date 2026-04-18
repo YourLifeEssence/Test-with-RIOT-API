1915f9f8950b48440d1f4a13d1cabe815cdefc85
Sierra Grace <132104920+SierraGrace@users.noreply.github.com>
2026-04-18 23:25:54 +0300

    Added first user interface version

---
+++ test_riot_api/script.js
@@ -0,0 +1,17 @@
+const avatar = document.getElementById("avatar");
+const summoner_level = document.getElementById("summoner_level");
+const nickname = document.getElementById("nickname");
+const tagline = document.getElementById("tagline");
+const region = document.getElementById("region");
+
+const rank_img = document.getElementById("rank_img");
+const tier = document.getElementById("tier");
+const lp = document.getElementById("lp");
+
+const name_search = document.getElementById("name_search");
+const tag_search = document.getElementById("tag_search");
+const search_btn = document.getElementById("search_btn");
+
+search_btn.addEventListener("click", () => {
+    //Write your code here
+});
